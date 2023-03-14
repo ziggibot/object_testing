98bff381fe5eee73eb11b06fbf8a29bd13724d48
ziggibot <till_kappus@gmx.de>
2023-03-14 11:46:41 +0100

    added working code to add and display new book objects

--- js/script.js
+++ js/script.js
@@ -1,48 +1,46 @@
 
-const testButton = document.getElementById("testbutton");
 
 const displayField = document.getElementById("displayfield");
-
-const nextBook = document.createElement("div");
-
-let newBook = "";
-let bookList = "";
+const buttonAddBook = document.getElementById("buttonaddbook");
 
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        (title + " by " + author + ", " + pages + " pages , " + read)
+    this.info = function () {
+        return (title + " by " + author + ", " + pages + " pages , " + read)
     }
 }
 
 const sunsetPark = new Book('Sunset Park', 'Paul Auster', '295', 'already read');
 const stiller = new Book('Stiller', 'Max Frisch', '105', 'not read yet');
-const derEkel = new Book('Der Ekel', 'Jean-Paul Sartre', '205', 'not read yet'); 
+const derEkel = new Book('Der Ekel', 'Jean-Paul Sartre', '205', 'not read yet');
 
 
-let myLibrary = [
-    sunsetPark,
-    stiller,
-    derEkel,
-];
 
-function addBookToLibrary() {
+let myLibrary = [];
 
-}
-
-function displayBooks() {
-    for (let i = 0; i < myLibrary.length; i++) {
-        bookList = (myLibrary[i]);
-        displayField.innerHTML = bookList;
+buttonAddBook.addEventListener('click', function() {
+    while (displayField.firstChild) {
+        displayField.removeChild(displayField.firstChild);
     }
-}
+    let newBookTitle = document.getElementById("newbooktitle").value;
+    let newBookAuthor = document.getElementById("newauthorname").value;
+    let newBookpages = document.getElementById("newpagenumber").value;
+    let newBookread = document.getElementById("newreadit").value;
+    const bookToAdd = new Book(newBookTitle, newBookAuthor, newBookpages, newBookread);
+    myLibrary.push(bookToAdd);
+    myLibrary.forEach(function (element) {
+        let nextBook = document.createElement("div");
+        nextBook.innerHTML = (element.title + ", " + element.author + ", " + element.pages + " pages, " + element.read)
+        displayField.appendChild(nextBook);
+    })
+});
 
 
 
-/* testButton.addEventListener('click', displayBooks); */
+
 
 
 
